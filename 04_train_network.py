2e26582cb4224e521e0bacdac1e63a6b7096fe35
Georg Schramm (Windows) <georg.schramm@kuleuven.be>
2025-05-19 08:48:54 +0200

    add model save

--- 04_train_network.py
+++ 04_train_network.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from pathlib import Path
+from datetime import datetime
 
 from data_utils import BrainwebLMPETDataset, brainweb_collate_fn
 from utils import LMNegPoissonLogLGradientLayer
@@ -12,11 +13,11 @@ class MiniConvNet(nn.Module):
     def __init__(self, in_channels=1, out_channels=1, num_features=16):
         super().__init__()
         self.conv = nn.Sequential(
-            nn.Conv3d(in_channels, num_features, 1, padding="same"),
-            nn.Conv3d(num_features, num_features, 1, padding="same"),
+            nn.Conv3d(in_channels, num_features, kernel_size=3, padding="same"),
+            nn.Conv3d(num_features, num_features, kernel_size=3, padding="same"),
             nn.PReLU(),
-            nn.Conv3d(num_features, num_features, 1, padding="same"),
-            nn.Conv3d(num_features, out_channels, 1, padding="same"),
+            nn.Conv3d(num_features, num_features, kernel_size=3, padding="same"),
+            nn.Conv3d(num_features, out_channels, kernel_size=3, padding="same"),
         )
 
     def forward(self, x):
@@ -34,32 +35,46 @@ class LMNet(nn.Module):
     def forward(
         self, x, lm_pet_lin_ops, contamination_lists, adjoint_ones, diag_preconds
     ):
+
+        # PET images can have arbitrary global scales, but we don't want to
+        # normalize before calculating the log-likelihood gradient
+        # instead we calculate scales of all images in the batch and apply
+        # them only before using the neural network
+
+        # as scale we use the mean of the input images
+        # if we are using early stopped OSEM images, the mean is well defined
+        # and stable
+
+        sample_scales = x.mean(dim=(2, 3, 4), keepdim=True)
+
         for i in range(self.n_blocks):
             x = x - self.lm_logL_grad_layer(
                 x, lm_pet_lin_ops, contamination_lists, adjoint_ones, diag_preconds
             )
-            x = nn.ReLU()(x - self.convs[i](x))
+            x = nn.ReLU()(x - self.convs[i](x / sample_scales) * sample_scales)
         return x
 
 
 if __name__ == "__main__":
     # input parameters
     seed = 42
-    num_epochs = 50
-    batch_size = 3
+    num_epochs = 2  # 28
+    batch_size = 6
     lr = 3e-4
     num_training_samples = 30
-    num_blocks = 4
+    num_blocks = 6  # number of unrolled blocks where each block does a data fidelity gradient step and a network step
 
     torch.manual_seed(seed)
 
-    # use first 20 data sets for training
     train_dirs = sorted(list(Path("data/sim_pet_data").glob("subject*")))[
         :num_training_samples
     ]
     train_dataset = BrainwebLMPETDataset(train_dirs, shuffle=True)
     train_loader = DataLoader(
-        train_dataset, batch_size=batch_size, collate_fn=brainweb_collate_fn
+        train_dataset,
+        batch_size=batch_size,
+        collate_fn=brainweb_collate_fn,
+        drop_last=True,
     )
 
     # 4. Training setup
@@ -68,12 +83,21 @@ if __name__ == "__main__":
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     criterion = nn.MSELoss()
 
-    # 5. Training loop (1 epoch for demo)
+    # create a unique output directory for the model checkpoints, use the current datetime string in the directory name
+    run_id = datetime.now().strftime("%Y%m%d_%H%M%S")
+    model_dir = Path(f"checkpoints/lmnet_run_{run_id}")
+    model_dir.mkdir(parents=True, exist_ok=True)
 
+    # save model architecture
+    with open(model_dir / "lmnet_architecture.txt", "w", encoding="UTF8") as f:
+        f.write(str(model))
+    print(f"Model architecture saved to {model_dir / 'lmnet_architecture.txt'}")
+
+    # training loop
     model.train()
     for epoch in range(1, num_epochs + 1):
-        running_loss = 0.0
-        for batch_idx, batch in enumerate(train_loader, 1):
+        batch_losses = torch.zeros(len(train_loader))
+        for batch_idx, batch in enumerate(train_loader):
             x = batch["input"].to(device)
             target = batch["target"].to(device)
             lm_pet_lin_ops = batch["lm_pet_lin_ops"]
@@ -88,12 +112,24 @@ if __name__ == "__main__":
             loss = criterion(output, target)
             loss.backward()
             optimizer.step()
-            running_loss += loss.item()
+            batch_losses[batch_idx] = loss.item()
 
             print(
-                f"Epoch [{epoch}/{num_epochs}] Batch [{batch_idx}/{len(train_loader)}]",
+                f"Epoch [{epoch}/{num_epochs}] Batch [{batch_idx+1}/{len(train_loader)}] - tr. loss: {loss.item():.3E}",
                 end="\r",
             )
 
-        avg_loss = running_loss / len(train_loader)
-        print(f"\nEpoch [{epoch}/{num_epochs}] Average Training Loss: {avg_loss:.4f}")
+        loss_avg = batch_losses.mean().item()
+        loss_std = batch_losses.std().item()
+        print(
+            f"\nEpoch [{epoch}/{num_epochs}] tr. loss: {loss_avg:.3E} +- {loss_std:.3E}"
+        )
+
+        torch.save(
+            {
+                "model_state_dict": model.state_dict(),
+                "optimizer_state_dict": optimizer.state_dict(),
+                "epoch": epoch,
+            },
+            model_dir / f"lmnet_epoch_{epoch:04}.pth",
+        )
